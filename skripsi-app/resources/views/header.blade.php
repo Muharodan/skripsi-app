16b301308ac2edffe8e69b43f399ec2597b9a56a
Muharodan <muharodan14@gmail.com>
2021-08-18 18:05:22 +0700

    second commit

---
+++ skripsi-app/resources/views/header.blade.php
@@ -0,0 +1,8 @@
+<!-- Just an image -->
+<nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+        <img src="/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="">
+        <h2>Penilaian website menggunakan fuzzy AHP dan fuzzy TOPSIS</h2>
+    </a>
+
+</nav>
---
+++ skripsi-app/resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('master')
+@section("content")
+<h1>Home Page</h1>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<form class="w3-container">
+  <p>
+  <label>Alamat website</label>
+  <input class="w3-input" type="text"></p>
+</form>
+<button class="btn btn-primary"> fuzzy ahp </button>
+<button class="btn btn-primary"> fuzzy topsis </button>
+@endsection
