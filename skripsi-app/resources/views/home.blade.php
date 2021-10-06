7c6ba1c7f11e5eeaf09680d09548f64fb8c7ea9a
Muharodan <muharodan14@gmail.com>
2021-10-06 23:26:47 +0700

    perubahan tampilan dan pengaturan button

--- skripsi-app/resources/views/home.blade.php
+++ skripsi-app/resources/views/home.blade.php
@@ -1,13 +1,37 @@
 @extends('master')
 @section("content")
-<h1>Home Page</h1>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<form class="w3-container">
-  <p>
-  <label>Alamat website</label>
-  <input class="w3-input" type="text"></p>
-</form>
-<button class="btn btn-primary"> fuzzy ahp </button>
-<button class="btn btn-primary"> fuzzy topsis </button>
+<!-- <h1>Home Page</h1> -->
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+<button class="btn btn-primary" name="fuzzy ahp">
+  <a class="fuzzyahp" href="hasil.blade.php">fuzzy ahp </a>
+</button>
+<button class="btn btn-primary" name="fuzzy topsis"> fuzzy topsis </button>
 @endsection
