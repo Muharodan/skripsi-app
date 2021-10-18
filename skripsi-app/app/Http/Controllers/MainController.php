9a31cc58de4f474642f4119697eb19b2428d3b24
Muharodan <Muharodan14@gmail.com>
2021-10-18 21:14:06 +0700

    membuat fungsi untuk upload dan load file csv

--- skripsi-app/app/Http/Controllers/MainController.php
+++ skripsi-app/app/Http/Controllers/MainController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Http\UploadedFile;
+use Importer;
 
 class MainController extends Controller
 {
@@ -11,10 +13,26 @@ class MainController extends Controller
     public function index(Request $request){
         $dateTime = date('Ymd_His');
         $file = $request->file('file');
-        // dd($file);
-        // print_r($file);
         $fileName = $dateTime.'-'.$file->getClientOriginalName();
         $savePath = public_path('/upload/');
         $file->move($savePath, $fileName);
+
+        $excel = Importer::make("CSV");
+        $excel->load($savePath.$fileName);
+        $collection = $excel->getCollection();
+
+        // if(sizeof($collection[1])==5){
+            for($i=1; $i<sizeof($collection); $i++){
+                try{
+                    print_r($collection[$i]);
+                    print("<br>");
+                }catch(Exception $e){
+
+                }
+            }
+        // }else{
+        //     print("Masuk Else");
+        // }
+
     }
 }
