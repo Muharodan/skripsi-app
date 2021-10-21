f09d40738c2050858183e08d3594f637a270daff
Muharodan <Muharodan14@gmail.com>
2021-10-21 13:06:28 +0700

    memperbaiki bug pada perhitungan geomtric mean

--- skripsi-app/app/Http/Controllers/FAHPController.php
+++ skripsi-app/app/Http/Controllers/FAHPController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Arr;
 
 class FAHPController extends Controller{
     private $pairwise;
@@ -18,9 +18,17 @@ class FAHPController extends Controller{
         // header size idx 2
         // criteria weights idx 4
         $this->pairwise = array(
-            array(1,2/1,4/1),
-            array(1/2,1,3/1),
-            array(1/4,1/3,1),
+            //Sidik
+            // array(1,2/1,4/1),
+            // array(1/2,1,3/1),
+            // array(1/4,1/3,1),
+
+            //Youtube
+            array(1,5,4,7),
+            array(1/5,1,1/2,3),
+            array(1/4,2,1,3),
+            array(1/7,1/3,1/3,1),
+
         );
 
         $this->fuzzyNumber = array(
@@ -35,15 +43,43 @@ class FAHPController extends Controller{
             array(9,9,9),//9
         );
 
+        // bacanya kanan ke kiri
         $this->pairwise = $this->changeToFuzzyNumber($this->pairwise, $this->fuzzyNumber);
-        
-        $this->calculateGeomecricMean($this->pairwise);
-        
-        // foreach($this->pairwise as $p){
+        print("PAIRWISE SETELAH CHANGE TO FUZZY NUMBER ". "DENGAN UKURAN MATRIX ".count($this->pairwise) ."<br>");
+        $this->print($this->pairwise);
+
+        print("<br>GEOMETRIC MEAN<br>");
+        //bacanya atas ke bawah
+        $this->geometricMean = $this->calculateGeomecricMean($this->pairwise);
+        $this->print($this->geometricMean);
+
+        // print("<br>INVERS TOTAL GEOMECTRIC MEAN <br>");
+        // print_r($this->calculateFuzzyWeight($this->geometricMean));
+
+        // print("<br>GEOMETRIC MEAN TRANSPOSE <br>");
+        // // //bacanya kanan ke kiri
+        // $this->geometricMean=$this->transpose($this->geometricMean);
+        // $this->print($this->geometricMean);
+        // foreach($this->geometricMean as $p){
         //     print_r($p);
         //     print("<br>");
         // }
         
+    }
+
+    private function print($matrix){
+        $size = count($matrix);
+        print("<table style='border: 1px solid black;'>");
+        for($i=0;$i<$size;$i++){
+            print("<tr style='border: 1px solid black;'>");
+            for($j=0;$j<count($matrix[$i]);$j++){
+                print("<td style='border: 1px solid black;'>");
+                    print_r($matrix[$i][$j]);
+                print("</td>");
+            }
+            print("<tr>");
+        }
+        print("</table>");
     }
 
     private function changeToFuzzyNumber($matrix, $fuzzyNumber){
@@ -80,17 +116,82 @@ class FAHPController extends Controller{
     }
 
     private function calculateGeomecricMean($matrix){
-        $size = count($matrix);
+        $size=count($matrix);
+        $temp=[];
+        $res=[];
         for($i=0;$i<$size;$i++){
-            for($j=0;$j<$size;$j++){
-                for($k=0;$k<$size;$k++){
-                    print_r($matrix[$i][$j][$k]);
-                    print(" ");  
-                }  
-                print("<br>");
-            }
-            print("<br>");
+            $res[$i]=[];
+            $temp[$i]=[];
         }
+
+        // masukin nilai ke temp
+        // dalam satu baris berisikan 
+        foreach($matrix as $row){
+            foreach($row as $col){
+                $i = 0;
+                foreach($col as $value){
+                    array_push($temp[$i], $value);
+                    $i++;
+                }
+            }
+        }
+        foreach($temp as $row){
+            $i=0;
+            $idx = 0;
+            $count=1;
+            foreach($row as $val){
+                $idx++;
+                $count*=$val;
+                if($idx == $size){
+                    array_push($res[$i],pow($count, 1/$size));
+                    $idx=0;
+                    $i++;
+                    $count=1;
+                    
+                }
+            }
+        }
+        return $res;
+    }
+
+    /**
+     * Calculate Fuzzy Weight 
+     * from Geometric Mean
+     */
+    private function calculateFuzzyWeight($matrix){
+        $size=count($matrix);
+        $res=[];
+
+        //calculate sum
+        for($i=0;$i<$size;$i++){
+            $temp=0;
+            for($j=0;$j<$size;$j++){
+                $temp+=$matrix[$i][$j];
+            }
+            array_push($res,$temp);
+        }
+
+        // calculate inverse
+        for($i=0,$j=$size-1;$i<$size,$j>=0;$i++,$j--){
+            $temp = $res[$i];
+            $res[$i]=$res[$j];
+            $res[$j]=$temp;
+        }
+
+
+        return $res;
+    }
+
+    private function transpose($matrix){
+        $size=count($matrix);
+        $res=[];
+        for($i=0;$i<$size;$i++){
+            for($j=0;$j<$size*$size;$j++){
+                $res[$i][$j]=0;
+                $res[$i][$j]=$matrix[$j][$i];
+            }
+        }
+        return $res;
     }
 
     /**
@@ -118,24 +219,6 @@ class FAHPController extends Controller{
 
         return $matrix;
     }
-
-    /**
-     * Calculate Creiteria Weights
-     * from normalize matrix
-     */
-    private function calculateCriteriaWeight($matrix){
-        $temp=0;
-        for($i=0;$i<3;$i++){
-            for($j=0;$j<3;$j++){
-                $temp+=$matrix[$i][$j];
-            }
-            $matrix[$i][3]=$temp/3;
-            $temp=0;
-        }
-        return $matrix;
-    }
-
-    
 
     //membuat matrix triangular fuzzy number
     function FuzzyPairwise(array $pairwise){
