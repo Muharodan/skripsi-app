8ea47d3c3c0219a82b07a50aecf89bd9159f247b
Muharodan <Muharodan14@gmail.com>
2021-11-15 15:38:27 +0700

    menambahkan migration untuk data web

---
+++ skripsi-app/database/migrations/2021_11_15_083023_create_webs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWebsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('webs', function (Blueprint $table) {
+            $table->id();
+            $table->string("nama_web");
+            $table->integer("broken_link");
+            $table->integer("page_load_time");
+            $table->integer("size_web");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('webs');
+    }
+}
