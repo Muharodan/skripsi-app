08d010d5d1d36521733bc1dd712ed151cd5c77b5
Muharodan <Muharodan14@gmail.com>
2021-11-15 21:31:08 +0700

    Menambahkan fungsi untuk mendapatkan broken_link, page_load_time, size_web, dan nama_web

--- skripsi-app/app/Http/Controllers/WebController.php
+++ skripsi-app/app/Http/Controllers/WebController.php
@@ -3,8 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class WebController extends Controller
 {
     //
+
+    public function getBrokenLink(){
+        return DB::select("
+            select broken_link
+            from webs
+        ");
+    }
+
+    public function getPageLoadTime(){
+        return DB::select("
+            select page_load_time
+            from webs
+        ");
+    }
+
+    public function getSizeWeb(){
+        return DB::select("
+            select size_web
+            from webs
+        ");
+    }
+
+    public function getName($id){
+        return DB::table('webs')->where('id', $id+1)->value('nama_web');
+        
+    }
 }
