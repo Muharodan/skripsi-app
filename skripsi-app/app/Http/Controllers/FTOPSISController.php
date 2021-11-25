75959ce1664cea55894352dd72acba5c7be6f19b
Muharodan <Muharodan14@gmail.com>
2021-11-26 00:01:50 +0700

    add

--- skripsi-app/app/Http/Controllers/FTOPSISController.php
+++ skripsi-app/app/Http/Controllers/FTOPSISController.php
@@ -28,41 +28,10 @@ class FTOPSISController extends Controller
             [1, 2, 1], //can4 
         ];
 
-        $data1=[
-            [3, 4, 3], //can1
-            [3, 3, 2], //can2
-            [4, 2, 0], //can3
-            [1, 2, 0], //can4 
-        ];
-
-        $data2=[
-            [2, 3, 3], //can1
-            [3, 2, 2], //can2
-            [3, 2, 1], //can3
-            [0, 1, 0], //can4 
-        ];
-
-        print("DATA 0");
         $data = $this->changeToFuzzyNumber($data, $fuzzyNumber);
         $this->print($data);
+
         print("<br>");
-
-        print("DATA 1");
-        $data1 = $this->changeToFuzzyNumber($data1, $fuzzyNumber);
-        $this->print($data1);
-        print("<br>");
-
-        print("DATA 2");
-        $data2 = $this->changeToFuzzyNumber($data2, $fuzzyNumber);
-        $this->print($data2);
-        print("<br>");
-
-        print("COMBINE DATA");
-        $data = $this->combine($data, $data1, $data2);
-        $this->print($data);
-        print("<br>");
-
-
         print("Weightage");
         $weightage = $this->changeToFuzzyNumber($weightage, $fuzzyNumber);
         $this->print($weightage);
@@ -93,41 +62,6 @@ class FTOPSISController extends Controller
         $data = $this->weightedNormalize($data, $weightage);
         $this->print($data);
         print("<br>");
-
-        $fpis=$this->FPIS($data);
-        print("FPIS");
-        $this->print($fpis);
-        print("<br>");
-
-        $fnis=$this->FNIS($data);
-        print("FNIS");
-        $this->print($fnis);
-        print("<br>");
-
-        $cc = $this->closenessCoefficient($fpis, $fnis);
-        $res=[];
-        for($i=0;$i<count($cc);$i++){
-            $res["id-".($i)]=$cc[$i];
-        }
-
-        arsort($res);
-        print_r($res);
-    }
-
-    private function combine($data, $data1, $data2){
-        for($i = 0; $i<count($data); $i++){
-            for($j = 0; $j<count($data[$i]); $j++){
-                // for($k = 0; $k<3; $k++){
-                    $temp =[
-                        min($data[$i][$j][0], $data1[$i][$j][0], $data2[$i][$j][0]),
-                        ($data[$i][$j][1] + $data1[$i][$j][1] + $data2[$i][$j][1])/3,
-                        max($data[$i][$j][2], $data1[$i][$j][2], $data2[$i][$j][2]),
-                    ];
-                // }
-                $data[$i][$j]= $temp;
-            }
-        }
-        return $data;
     }
 
     private function changeToFuzzyNumber($data, $fuzzyNumber){
@@ -208,105 +142,5 @@ class FTOPSISController extends Controller
             }
         }
         return $data;
-    }
-
-    private function FPIS($data){
-        $max = [
-            PHP_INT_MIN,
-            PHP_INT_MIN,
-            PHP_INT_MIN,
-        ];
-        $res = [
-            [],
-            [],
-            []
-        ];
-        for($i = 0; $i<count($data); $i++){
-            for($j = 0; $j<count($data[$i]); $j++){
-                // for($k =0; $k<3; $k++){
-                    // print("I: ".$i." J: ".$j." Data: ".$data[$i][$j][2]. " Max[".$j."]: ".$max[$j]);
-                    // print("<br>");
-                    if($data[$i][$j][2] >= $max[$j]){
-                        $max[$j] = $data[$i][$j][2];
-                        $res[$j] = $data[$i][$j];
-                        if($data[$i][$j][1] >= $res[$j][1]){
-                            $res[$j] = $data[$i][$j];
-                            if($data[$i][$j][0] >= $res[$j][0]){
-                                $res[$j] = $data[$i][$j];
-                            }
-                        }
-                    }
-                    // print("I: ".$i." J: ".$j." Data: ".$data[$i][$j][2]. " Max[".$j."]: ".$max[$j]);
-                    // print("<br>");
-                    // print("<br>");
-                // }
-            }
-        }
-        return $this->calculateDistance($data, $res);
-    }
-
-    private function FNIS($data){
-        $max = [
-            PHP_INT_MAX,
-            PHP_INT_MAX,
-            PHP_INT_MAX,
-        ];
-        $res = [
-            [],
-            [],
-            []
-        ];
-        for($i = 0; $i<count($data); $i++){
-            for($j = 0; $j<count($data[$i]); $j++){
-                // for($k =0; $k<3; $k++){
-                    // print("I: ".$i." J: ".$j." Data: ".$data[$i][$j][2]. " Min[".$j."]: ".$max[$j]);
-                    // print("<br>");
-                    if($data[$i][$j][2] <= $max[$j]){
-                        $max[$j] = $data[$i][$j][2];
-                        $res[$j] = $data[$i][$j];
-                        if($data[$i][$j][1] <= $res[$j][1]){
-                            $res[$j] = $data[$i][$j];
-                            if($data[$i][$j][0] <= $res[$j][0]){
-                                $res[$j] = $data[$i][$j];
-                            }
-                        }
-                    }
-                    // print("I: ".$i." J: ".$j." Data: ".$data[$i][$j][2]. " Min[".$j."]: ".$max[$j]);
-                    // print("<br>");
-                    // print("<br>");
-                // }
-            }
-        }
-        
-        return $this->calculateDistance($data, $res);
-    }
-
-    private function calculateDistance($data, $res){
-        for($i = 0; $i<count($data); $i++){
-            $sum = 0;
-            for($j = 0; $j<count($data[$i]); $j++){
-                $temp = 0;
-                for($k =0; $k<3; $k++){
-                    // print("I: ".$i." J: ".$j." Data: ".$data[$i][$j][$k]. " Res: ".$res[$j][$k]);
-                    // print("<br>");
-                    $temp+=pow($data[$i][$j][$k]-$res[$j][$k],2);
-                }
-                // print("Temp: ".$temp);
-                // print("<br>");
-                $temp = sqrt($temp/3);
-                $sum+=$temp;
-                $data[$i][$j]=$temp;
-            }
-            $data[$i][3]=$sum;
-        }
-        return $data;
-    }
-
-    private function closenessCoefficient($fpis, $fnis){
-        $res = [];
-        for($i = 0; $i<count($fpis); $i++){
-            $res[$i]=$fnis[$i][3]/($fnis[$i][3]+$fpis[$i][3]);
-        }
-        return $res;
     }
 }
