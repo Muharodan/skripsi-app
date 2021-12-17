c9333cc18bae7600a7d9fca398d2268c1dbad798
Muharodan <Muharodan14@gmail.com>
2021-12-17 11:03:48 +0700

    menambahkan idKategori beserta setter, mengubah dan menambahkan getter untuk data untuk handle idKategori yang dipilih user

--- skripsi-app/app/Http/Controllers/WebController.php
+++ skripsi-app/app/Http/Controllers/WebController.php
@@ -2,45 +2,73 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Web;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class WebController extends Controller
 {
-    //
+    private KategoriController $kategoriController;
+    private $idKategori;
+    public function __construct()
+    {
+        $this->kategoriController = new KategoriController();
+    }
+
+    public function setIdKategori($id){
+        $this->idKategori = $id;
+    }
 
     public function index(){
         $listWeb = DB::table('webs')->paginate(10);
 
-        return view('home', ['listWeb'=>$listWeb]);
+        $kategori = $this->kategoriController->index();
+
+        return view('home', ['listWeb'=>$listWeb, 'kategori'=>$kategori]);
     }
 
     public function getBrokenLink(){
-        return DB::select("
-            select broken_link
-            from webs
-        ");
+        if($this->idKategori==0){
+            $data = Web::select('broken_link')->get();
+        }else{
+            $data = Web::select('broken_link')
+            ->where('id_kategori', $this->idKategori)
+            ->get();
+        }
+        return $data;
     }
 
     public function getPageLoadTime(){
-        return DB::select("
-            select page_load_time
-            from webs
-        ");
+        if($this->idKategori==0){
+            $data = Web::select('page_load_time')->get();
+        }else{
+            $data = Web::select('page_load_time')
+            ->where('id_kategori', $this->idKategori)
+            ->get();
+        }
+        return $data;
     }
 
     public function getSizeWeb(){
-        return DB::select("
-            select size_web
-            from webs
-        ");
+        if($this->idKategori==0){
+            $data = Web::select('size_web')->get();
+        }else{
+            $data = Web::select('size_web')
+            ->where('id_kategori', $this->idKategori)
+            ->get();
+        }
+        return $data;
     }
 
     public function getAll(){
-        return DB::select("
-            select broken_link, page_load_time, size_web
-            from webs
-        ");
+        if($this->idKategori==0){
+            $data = Web::select('broken_link', 'page_load_time', 'size_web')->get();
+        }else{
+            $data = Web::select('broken_link', 'page_load_time', 'size_web')
+            ->where('id_kategori', $this->idKategori)
+            ->get();
+        }
+        return $data;
     }
 
     public function find($id){
